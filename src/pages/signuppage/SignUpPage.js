d08cee242daac30bdccab7f1335284755656ccae
devloliconic <kirillevsmirnov@gmail.com>
2022-07-10 00:36:32 +0700

    create regform

--- src/pages/signuppage/SignUpPage.js
+++ src/pages/signuppage/SignUpPage.js
@@ -3,13 +3,16 @@ import "./signupglobal/SignUpGlobal.scss"
 import Header from "./header/SignUpHeader"
 import Footer from "./footer/SignUpFooter"
 import Main from "./main/SignUpMain"
+import RegForm from "./main/RegForm"
 
 
 function SignUpPage() {
     return (
-      <div className=""></div>
+      <div className="container">
+        <RegForm></RegForm> 
+      </div>
     );
   }
   
-  export default SignUpPage;
+export default SignUpPage;
   
