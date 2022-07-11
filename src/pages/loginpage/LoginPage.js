0ca7af2d2d3ac57962579dc52f64be8c475866e9
devloliconic <kirillevsmirnov@gmail.com>
2022-07-12 00:55:39 +0700

    add login page

--- src/pages/loginpage/LoginPage.js
+++ src/pages/loginpage/LoginPage.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+
+
 import "normalize.css"
 import "./loginglobal/LoginGlobal.scss"
 import HeaderTop from "./header/LoginHeader"
@@ -6,10 +11,49 @@ import Main from "./main/LoginMain"
 
 
 const LoginPage = () => {
+    const SignupSchema = Yup.object().shape({
+      email: Yup.string().email("Неправильная почта"),
+      password: Yup.string(),
+    })
     return (
-      <div>
-        тут
+      <div className='login'>
+        <Formik
+          initialValues={{
+            email: " ",
+            password: " ",
+          }}
+          validationSchema={SignupSchema}
+          validateOnBlur={SignupSchema}
+          onSubmit = {values => {console.log(values);}}
+        >
+          {({values, errors, handleBlur, isValid, dirty, touched, handleChange, handleSubmit,  isSubmitting}) => (
+            <Form>
+              <h2 className='login__logo'>Вход</h2>
+              <p className='login__text'>Почта</p>
+              {errors.email && touched.email  && <p className='login__error'>{errors.email}</p>}
+              <input type="email" 
+              name="email"
+              className='login__input'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+              />
+              <p className='login__text'>Пароль</p>
+              <input
+              type ="password"
+              className='login__input'
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+              />
+              <button type='submit' onClick={handleSubmit} disabled={!isValid || isSubmitting} className='login__button'>Войти</button>
+            </Form>
+          )}
+        </Formik>
       </div>
+
+      
     );
   }
   
