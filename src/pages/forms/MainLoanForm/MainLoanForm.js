31f18a77c5d6b23add4a5d8f80ce5ce71a8caa89
valerka12031 <kirichenko7888@gmail.com>
2022-07-12 20:57:56 +0700

    update Main Loan Form

--- src/pages/forms/MainLoanForm/MainLoanForm.js
+++ src/pages/forms/MainLoanForm/MainLoanForm.js
@@ -1,78 +1,65 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import TestSlider from './input-range/input-range';
+import Slider from './input-range/input-range';
 
 import './MainLoanForm.scss'
 
 const TakeLoanForm = () => {
 
-    const [sliderSummValue, setSliderSummValue] = useState('1000');
-    const [sliderDaysValue, setSliderDaysValue] = useState('30');
-
     const [radioButtonValue, setRadioButtonValue] = useState('');
+
+    const [isRadioButtonPackageValid, setIsRadioButtonPackageValid] = useState(false);
+    
 
     const [isFormValid, setIsFormValid] = useState(false);
 
-    function setSliderValue(slider){
-        switch(slider.target.className){
-            case 'summ-input' : setSliderSummValue(slider.target.value); break;
-            case 'days-input' : setSliderDaysValue(slider.target.value); break;
-        }
-    }
+    useEffect(()=>{
+        setRadioButtonValue(radioButtonValue);
+        setIsRadioButtonPackageValid(true);
+        console.log("package-name: " + radioButtonValue);
+        console.log("package-active: " + isRadioButtonPackageValid);
+        },
+        [radioButtonValue,isRadioButtonPackageValid]
+    )
+
+    
 
     function setPackage(){
             if(document.getElementById('social').checked) {
-                setRadioButtonValue('Социальный');
-                setIsFormValid(true);
+                setRadioButtonValue('Социальный'); 
             } 
             else if(document.getElementById('bomj').checked){
                 setRadioButtonValue('Бомж');
-                setIsFormValid(true);   
             }
             else if(document.getElementById('chill').checked){
                 setRadioButtonValue('Чилл');
-                setIsFormValid(true);
             }
+            
+            formValidation();
         }
     
+        function formValidation(){
+                if(isRadioButtonPackageValid){
+                    setIsFormValid(true);
+                }
+        }
+
     return (
         <form className="form-container">
-            <div className='form-container__summ-block form-container__item'>
-                <div className='top-text text'>
-                    <label>Выберите сумму</label>
-                    <label>{sliderSummValue}р</label>
-                </div>
-                <div className='form-container__slider'>
-                            <input 
-                            type='range' 
-                            onChange={e => {setSliderValue(e)}}
-                            className='summ-input' 
-                            min='1000' 
-                            max='15000'
-                            ></input>
-                </div>
-                <div className='bot-text text'>
-                    <label>1000 р</label>
-                    <label>15000 р</label>
-                </div>
-            </div>
-            <div className='form-container__days-block form-container__item'>
-                <div className='top-text text'>
-                    <label>Выберите срок</label>
-                    <label>{sliderDaysValue} дней</label>
-                </div>
-                <div className='form-container__slider'>
-                            <input 
-                            type='range' 
-                            onChange={e => {setSliderValue(e)}}
-                            className='days-input' 
-                            min='1' 
-                            max='30'
-                            ></input>
-                </div>
-                <div className='bot-text text'>
-                    <label>1 дней</label>
-                    <label>30 дней</label> 
-                </div>
-            </div>
+            <Slider 
+            title="Выберите сумму" 
+            defaultValue = '3000'
+            units = 'р'
+            minValue='3000' 
+            maxValue = '30000' 
+            />
+            <Slider 
+            title="Выберите срок" 
+            defaultValue = '50' 
+            units = 'дней'
+            minValue='1' 
+            maxValue ='60' 
+            />
             <div className='form-container__packages-block form-container__item'>
                 <div className='top-text text'>
                     <label>Выберите пакет</label>
@@ -81,20 +68,20 @@ const TakeLoanForm = () => {
                 <div className='packages-block__item'>
                     <label className='items-label' htmlFor='social'>● Социальный</label>
                     <input type='radio' name = 'pack_name' id='social' value='Социальный' 
-                    onChange={e =>{setPackage()}}
+                    onClick={e =>{setPackage()}}
                     
                     ></input>
                 </div>
                 <div className='packages-block__item'>
                     <label className='items-label' htmlFor='bomj'>● Бомж</label>
                     <input type='radio' name = 'pack_name' id='bomj' value='Бомж'
-                    onChange={e =>{setPackage()}}
+                    onClick={e =>{setPackage()}}
                     ></input>
                 </div>
                 <div className='packages-block__item'>
                     <label className='items-label' htmlFor='chill'>● Чилл</label>
                     <input type='radio' name = 'pack_name' id='chill' value='Чилл'
-                    onChange={e =>{setPackage()}}
+                    onClick={e =>{setPackage()}}
                     ></input>
                 </div>
             </div>
---
+++ src/pages/forms/MainLoanForm/input-range/input-range.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+import './inpit-range.scss'
+
+
+
+
+export default function Slider(props) {
+
+    const [sliderValue, setSliderValue] = useState('');
+
+    function setRangeInput(slider){
+         setSliderValue(slider.target.value); console.log(sliderValue);
+    }
+
+
+  return (
+    <div className='form-container'>
+                <div className='form-container__top-text text'>
+                    <label>{props.title}</label>
+                    {(sliderValue!='') && <label>{sliderValue} {props.units}</label>}
+                    {(sliderValue =='') && <label>{props.defaultValue} {props.units}</label>}
+                </div>
+                <div className='form-container__range-input'>
+                            <input 
+                            type='range' 
+                            onChange={e => {setRangeInput(e)}}
+                            className='summ-input' 
+                            min={props.minValue}
+                            max={props.maxValue}
+                            ></input>
+                </div>
+                <div className='bot-text text'>
+                    <label>{props.minValue}</label>
+                    <label>{props.maxValue}</label>
+                </div>
+            </div>
+  )
+}
+
+
+
+// export default function TestSlider(props){
+
+
+//     const [sliderSummValue, setSliderSummValue] = useState('1000');
+//     const [sliderDaysValue, setSliderDaysValue] = useState('30');
+
+//     const [radioButtonValue, setRadioButtonValue] = useState('');
+
+//     const [isRadioButtonPackageValid, setIsRadioButtonPackageValid] = useState(false);
+    
+
+//     const [isFormValid, setIsFormValid] = useState(false);
+
+//     useEffect(()=>{
+//         setRadioButtonValue(radioButtonValue);
+//         setIsRadioButtonPackageValid(true);
+//         console.log("package-name: " + radioButtonValue);
+//         console.log("package-active: " + isRadioButtonPackageValid);
+//         },
+//         [radioButtonValue,isRadioButtonPackageValid]
+//     )
+
+//     function setSliderValue(slider){
+//         switch(slider.target.className){
+//             case 'summ-input' : setSliderSummValue(slider.target.value); console.log(sliderSummValue); break;
+//             case 'days-input' : setSliderDaysValue(slider.target.value); console.log(sliderDaysValue); break;
+//         }
+//     }
+
+//     function setPackage(){
+//             if(document.getElementById('social').checked) {
+//                 setRadioButtonValue('Социальный'); 
+//             } 
+//             else if(document.getElementById('bomj').checked){
+//                 setRadioButtonValue('Бомж');
+//             }
+//             else if(document.getElementById('chill').checked){
+//                 setRadioButtonValue('Чилл');
+//             }
+            
+//             formValidation();
+//         }
+    
+//         function formValidation(){
+//                 if(isRadioButtonPackageValid){
+//                     setIsFormValid(true);
+//                 }
+//         }
+
+
+
+//     return(
+//         <div className='form-container'>
+//                 <div className='top-text text'>
+//                     <label>{props.title}</label>
+//                     <label>{sliderDaysValue} дней</label>
+//                 </div>
+//                 <div className='form-container__slider'>
+//                             <input 
+//                             type='range' 
+//                             onChange={e => {setSliderValue(e)}}
+//                             className='days-input' 
+//                             min='1' 
+//                             max='30'
+//                             ></input>
+//                 </div>
+//                 <div className='bot-text text'>
+//                     <label>1 дней</label>
+//                     <label>30 дней</label> 
+//                 </div>
+//             </div>
+//     )
+// }
