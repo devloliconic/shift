8b1ef5badfed1d335212f5a2c97f6259e18688d9
valerka12031 <kirichenko7888@gmail.com>
2022-07-14 20:52:39 +0700

    changed regPage design

--- src/pages/regpage/RegPage.js
+++ src/pages/regpage/RegPage.js
@@ -7,7 +7,7 @@ import RegMain from "./main/RegMain"
 import Main from "./main/RegMain"
 import React, {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
-
+import MainInfo from "./main/info"
 
 function RegPage({isLoggedIn}) {
     let navigate = useNavigate();
@@ -19,11 +19,35 @@ function RegPage({isLoggedIn}) {
     if(isLoggedIn) {
         return (
             <div className="core-container">
-              <div className="core-container__main-text">
-                <label>ВОЗЬМИ ПЕРВЫЙ ЗАЙМ</label>
-                <label className="core-container__main-text__free">БЕСПЛТАНО</label>
-              </div>
-              <RegMain className="core-container__main-form" />
+              <div className = "core-container__item">
+                <div className="core-container__item__main-text">
+                    <label>ВОЗЬМИ ПЕРВЫЙ ЗАЙМ</label>
+                    <label className="core-container__item__main-text__free">БЕСПЛАТНО</label>
+                </div>
+                <RegMain className="core-container__item__main-form" />
+              </div> 
+                <div className="core-container__item">
+                    <div className="core-container__item__bot">
+                        <div className = "core-container__item__bot__other-title" >Другие наши услуги:</div>
+                        <div className = "core-container__item__bot__benefits__top">
+                            <div className = "core-container__item__bot__benefits__top__1">
+                            <div className = "core-container__item__bot__benefits__top__img1"></div>
+                            <span>Страхование жизни и здоровья</span>
+                            </div>
+                            <div className = "core-container__item__bot__benefits__top__2">
+                            Акции и валюта
+                            </div>
+                        </div>
+                        <div className = "core-container__item__bot__benefits__bot">
+                            <div className = "core-container__item__bot__benefits__bot__1">
+                            Ставки на спорт
+                            </div>
+                            <div className = "core-container__item__bot__benefits__bot__2">
+                            Ипотека
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
           );
     }
---
+++ src/pages/regpage/main/info.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainInfo() {
+  return (
+    <div>MainInfo</div>
+  )
+}
+
+export default MainInfo
